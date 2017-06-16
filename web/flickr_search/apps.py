d47f35ace263260c35951a5b9111de6779787aa9
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-06-16 13:22:48 +0300

    added app label

--- web/flickr_search/apps.py
+++ web/flickr_search/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy as _
 
 
 class FlickrSearchConfig(AppConfig):
     name = 'flickr_search'
+    verbose_name = _('Search')
