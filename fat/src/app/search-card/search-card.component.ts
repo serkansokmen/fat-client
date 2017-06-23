445fd2f4c2c2fe9c30a1c6704acf236bb2ec9b3f
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-06-23 21:06:38 +0300

    thumb scaling fixed

--- fat/src/app/search-card/search-card.component.ts
+++ fat/src/app/search-card/search-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, Input, OnInit, ChangeDetectionStrategy, ElementRef } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { SearchState } from '../reducers/search.reducer';
 import { SearchActions } from '../actions/search.actions';
@@ -15,13 +15,16 @@ export class SearchCardComponent implements OnInit {
 
   @Input() image: Image;
   @Input() viewMode: ViewMode;
+  @Input() thumbnailScale: number;
 
   constructor(
     private store: Store<SearchState>,
     private actions: SearchActions,
-  ) { }
+    private elementRef: ElementRef,
+  ) {}
 
   ngOnInit() {
+
   }
 
   getImageURL(image: Image) {
