ee99ffb3dfa27793a4e3d2b45907ba048d96ea97
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-06-23 21:53:32 +0300

    layout updates

--- fat/src/app/app.module.ts
+++ fat/src/app/app.module.ts
@@ -23,7 +23,6 @@ import { StoreModule } from '@ngrx/store';
 // import { routerReducer, RouterStoreModule } from '@ngrx/router-store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
 import { SafeStylePipe } from './pipes/safe-style.pipe';
 import { SearchCardComponent } from './search-card/search-card.component';
 
