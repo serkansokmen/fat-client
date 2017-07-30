2b8c98b4a89dca4431169c5568ca102973254693
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2017-07-30 15:49:51 +0300

    added staging / production envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   hmr: false,
-  adminURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/admin/',
-  apiURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/api/v1/',
-  authURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/auth/'
+  adminURL: 'http://fat-prod.us-west-2.elasticbeanstalk.com/admin/',
+  apiURL: 'http://fat-prod.us-west-2.elasticbeanstalk.com/api/v1/',
+  authURL: 'http://fat-prod.us-west-2.elasticbeanstalk.com/auth/'
 };
---
+++ src/environments/environment.staging.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  hmr: false,
+  adminURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/admin/',
+  apiURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/api/v1/',
+  authURL: 'http://fat-staging.us-west-2.elasticbeanstalk.com/auth/'
+};
